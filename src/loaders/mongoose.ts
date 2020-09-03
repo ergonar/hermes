62b8e127cb28867fc683a2442c01e11483f784f0
Ergo <termigiova@me.com>
2020-09-03 11:37:56 -0500

    Refactor loader to get a connection object and have the possibility to close it.

--- src/loaders/mongoose.ts
+++ src/loaders/mongoose.ts
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose, { Mongoose } from 'mongoose';
 import { Db } from 'mongodb';
 import config from '../config';
 
@@ -7,16 +7,35 @@ const databaseUrl = config.databaseUrl.replace(
   config.databasePassword
 );
 
-export default async (): Promise<Db> => {
-  try {
-    const connection = await mongoose.connect(databaseUrl, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-    });
+const connect = async (url: string): Promise<Mongoose> => {
+  const connection = await mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
 
+  return connection;
+};
+
+export const mongooseDatabase = async (
+  url: string = databaseUrl
+): Promise<Db> => {
+  try {
+    const connection = await connect(url);
     return connection.connection.db;
+  } catch (error) {
+    console.log('Error while connecting to mongo database');
+    throw new Error(error);
+  }
+};
+
+export const mongooseConnection = async (
+  url: string = databaseUrl
+): Promise<Mongoose> => {
+  try {
+    const connection = await connect(url);
+    return connection;
   } catch (error) {
     console.log('Error while connecting to mongo database');
     throw new Error(error);
