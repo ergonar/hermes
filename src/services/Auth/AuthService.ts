2fb6113112242d1f6566dc2a4bf22cf4be612091
Ergo <termigiova@me.com>
2020-09-17 18:28:37 -0500

    Add signup and signin functionality

--- src/services/Auth/AuthService.ts
+++ src/services/Auth/AuthService.ts
@@ -1,25 +1,27 @@
-import jwt from 'jsonwebtoken';
 import { Service } from 'typedi';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
 
-import UserInterface from '../../models/User/UserInterface';
-import User from '../../models/User/user';
-import UserDocument from '../../models/User/UserDocument';
-import config from '../../config';
 import { JWTInterface, JWTUserPayloadInterface } from './JWTInterface';
+import UserInterface from '../../models/User/UserInterface';
+import UserDocument from '../../models/User/UserDocument';
+import User from '../../models/User/user';
+import logger from '../../utils/winston';
+import config from '../../config';
 
 @Service()
 class AuthService {
-  public async decodeToken(token: string): Promise<JWTInterface> {
-    return await jwt.verify(token, config.jwtSecret);
-  }
-
   public async signup(
     userData: UserInterface
-  ): Promise<{ user: UserDocument; token: string }> {
+  ): Promise<{ user: UserInterface; token: string }> {
     try {
-      const user = User.build(userData);
-      await user.save();
-      const token = this.generateToken(user);
+      let userDocument = User.build(userData);
+      await userDocument.save();
+      const token = this.generateToken(userDocument);
+
+      const user = userDocument.toObject();
+      Reflect.deleteProperty(user, 'password');
+      Reflect.deleteProperty(user, 'active');
 
       return { user, token };
     } catch (error) {
@@ -27,14 +29,51 @@ class AuthService {
     }
   }
 
+  public async signin(
+    email: string,
+    password: string
+  ): Promise<{ user: UserInterface; token: string }> {
+    try {
+      const userDocument = await User.findOne({ email })
+        .select('+password')
+        .exec();
+      const passwordIsCorrect = await bcrypt.compare(
+        password,
+        userDocument.password
+      );
+
+      if (!passwordIsCorrect) {
+        throw new Error('Incorrect email or password');
+      }
+
+      const token = this.generateToken(userDocument);
+
+      const user = userDocument.toObject();
+      Reflect.deleteProperty(user, 'password');
+      Reflect.deleteProperty(user, 'active');
+
+      return { user, token };
+    } catch (error) {
+      logger.error('Error while signing in: ' + error.message);
+      throw new Error('Error while signing in: ' + error.message);
+    }
+  }
+
+  public forgotPassword() {}
+  public resetPassword() {}
+
   private generateToken(user: UserDocument) {
     const payload: JWTUserPayloadInterface = {
-      id: user.id,
+      _id: user.id,
       name: user.username,
     };
     return jwt.sign(payload, config.jwtSecret, {
       expiresIn: config.jwtExpiresIn,
     });
+  }
+
+  public async decodeToken(token: string): Promise<JWTInterface> {
+    return await jwt.verify(token, config.jwtSecret);
   }
 }
 
