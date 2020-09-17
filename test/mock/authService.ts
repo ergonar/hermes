bf55ee29dc8e222a5840060df5979685db8af2c6
Ergo <termigiova@me.com>
2020-09-17 18:29:27 -0500

    Upgrade signup user mock

--- test/mock/authService.ts
+++ test/mock/authService.ts
@@ -1,14 +1,25 @@
 import Container from 'typedi';
+import User from '../../src/models/User/user';
 import UserDocument from '../../src/models/User/UserDocument';
+import UserInterface from '../../src/models/User/UserInterface';
 import AuthService from '../../src/services/Auth/AuthService';
 import { createCorrectUserMock } from './userMock';
 
 export const createSignupUserMock = async (): Promise<{
   user: UserDocument;
+  userMock: UserInterface;
   token: string;
 }> => {
-  const authService = Container.get(AuthService);
-  const userMock = createCorrectUserMock();
-  const { user, token } = await authService.signup(userMock);
-  return { user, token };
+  try {
+    const authService = Container.get(AuthService);
+    const userMock = createCorrectUserMock();
+    const { user: createdUser, token } = await authService.signup(userMock);
+
+    const user = await User.findOne({ _id: createdUser._id })
+      .select('+password')
+      .exec();
+    return { user, userMock, token };
+  } catch (error) {
+    throw new Error('Error while creating signup user mock. ' + error.message);
+  }
 };
