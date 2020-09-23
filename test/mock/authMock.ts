0f5f5aa527064f6c22220d287538a831427b0431
Ergo <termigiova@me.com>
2020-09-23 17:59:02 -0500

    Create cookies mocks

--- test/mock/authMock.ts
+++ test/mock/authMock.ts
@@ -1,4 +1,6 @@
 import faker from 'faker';
+import { Response } from 'express';
+import setCookie from 'set-cookie-parser';
 
 import UserInterface from '../../src/models/User/UserInterface';
 
@@ -20,4 +22,27 @@ export const createIncompleteSignupUserMock = () => {
     // passwordConfirm: password,
     email: faker.internet.email(),
   };
+};
+
+export const createJWTExpirationDateMocks = () => {
+  const today = new Date();
+  let eightyNinthDay = new Date();
+  let ninetyFirstDay = new Date();
+  eightyNinthDay.setDate(today.getDate() + 89);
+  ninetyFirstDay.setDate(today.getDate() + 91);
+
+  return { eightyNinthDay, ninetyFirstDay };
+};
+
+export const getJWTCookieFromResponse = (response: Response) => {
+  const cookies = setCookie.parse(response, {
+    decodeValues: true,
+  });
+  const jwtCookie = cookies.find(cookie => {
+    if (cookie.name === 'jwt') {
+      return cookie;
+    }
+  });
+
+  return jwtCookie;
 };
