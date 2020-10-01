9cab2de45ccfd7b6321b6e2484d6c53e4b4c3705
Ergo <termigiova@me.com>
2020-10-01 10:37:23 -0500

    Delete non used file

--- src/loaders/errorHandler.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-import globalErrorHandler from '../controllers/errorController';
-
-export default ({ app }: { app: express.Application }) => {
-  app.use(globalErrorHandler);
-
-  return app;
-};
